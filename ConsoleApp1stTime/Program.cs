77b3f20dc6d71a9d204be51dea811cec8c9f4775
Sinead Jacobsson <sinead.jacobsson@gmail.com>
2020-09-07 10:20:07 +0200

    Test edit

---
+++ ConsoleApp1stTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1stTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
